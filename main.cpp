38753b23a0fc870615edf7b3802ac25baf268627
sebascm-dev <spanolocabo@gmail.com>
2025-03-18 22:24:12 +0100

    [+] EMPEZANDO DE NUEVO PARA COMPRENDER LOS PASOS Y MODULARIZAR CORRECTAMENTE

--- main.cpp
+++ main.cpp
@@ -8,85 +8,10 @@ using namespace std;
 #define N 10
 #define SALTO 4
 
-void menu1() {
-    int torneos = 0;
-    int decisionMenu1 = 0;
-
-    do {
-        system("cls");
-
-        cout << "CLUB DE GOLF" << endl;
-        cout << "------------------------------------" << endl;
-        cout << "Torneos: " << torneos << endl;
-        cout << endl;
-        cout << "\t 1. Listado Torneos Abiertos" << endl;
-        cout << "\t 2. Alta Torneo" << endl;
-        cout << "\t 3. Elegir Torneo" << endl;
-        cout << "\t 4. Salir" << endl;
-        cout << endl;
-        cout << "Indique la opcion deseada: ";
-
-        cin >> decisionMenu1;
-
-        switch (decisionMenu1) {
-
-            case 1:
-                system("cls");
-
-                cout << endl;
-                cout << "Listado de torneos abiertos:" << endl;
-                break;
-
-
-            case 2:
-                system("cls");
-
-                cout << endl;
-                cout << "Alta de nuevo torneo:" << endl;
-                break;
-
-
-            case 3:
-                system("cls");
-
-                cout << endl;
-                cout << "Eleccion de torneo:" << endl;
-                break;
-
-
-            case 4:
-                system("cls");
-
-                cout << endl;
-                cout << "Saliendo de la aplicacion..." << endl;
-                break;
-
-
-            default:
-                system("cls");
-
-                cout << endl;
-                cout << "Opcion no valida. Por favor, seleccione una opcion del 1 al 4." << endl;
-                break;
-        }
-
-        if (decisionMenu1 != 4) {
-            cout << endl;
-            cout << "Presione Enter para continuar...";
-            cin.ignore();
-            cin.get();
-        }
-
-    } while (decisionMenu1 != 4);
-}
-
-int main() {
-
-    //menu1();
-
+int main()
+{
     Torneo t1;
-    t1.CrearFichero("Sevilla P1");
-    t1.mostrar(14);
+    t1.CrearFichero("Premier Golf Miami P1");
 
     return 0;
 }
--- src/Torneo.cpp
+++ src/Torneo.cpp
@@ -9,9 +9,13 @@ using namespace std;
 Torneo::Torneo(){}
 Torneo::~Torneo(){}
 
+
+
 // Métodos Básicos
 int Torneo::getNumGolfistas() { return numGolfistas; }
 void Torneo::putNumGolfistas(int n) { numGolfistas = n; }
+
+
 
 // Gestión de Nombres
 void Torneo::getNomTorneo(cadena nombre) { strcpy(nombre, nomTorneo); }
@@ -19,9 +23,12 @@ void Torneo::getNomFichero(cadena nombre) { strcpy(nombre, nomFichero); }
 void Torneo::putNomTorneo(cadena nombre) { strcpy(nomTorneo, nombre); }
 void Torneo::putNomFichero(cadena nombre) { strcpy(nomFichero, nombre); }
 
+
+
 // Gestión de Ficheros
 void Torneo::CrearFichero(char nombreFichero[]) {
-    fstream fichero(nombreFichero, ios::binary | ios::in | ios::out);
+
+    fichero.open(nombreFichero, ios::binary | ios::in | ios::out);
 
     if(fichero.fail()) {
         fichero.clear();
@@ -33,76 +40,45 @@ void Torneo::CrearFichero(char nombreFichero[]) {
         fichero.close();
 
         fichero.open(nombreFichero, ios::binary | ios::in | ios::out);
-
+        cout << "[+] Fichero creado: " << nombreFichero << endl;
         cout << endl;
-        cout << "[+] El Fichero " << nombreFichero << " se ha creado correctamente!" << endl;
-
     } else {
-        cout << endl;
-        cout << "[!] El Fichero " << nombreFichero << " ya existe..." << endl;
-
-        cout << endl;
         fichero.seekg(0);
         fichero.read((char*) &numGolfistas, sizeof(int));
+        cout << "[!] Fichero existente: " << nombreFichero << endl;
+        cout << "[i] Numero de Golfistas: " << numGolfistas << endl;
+        cout << endl;
     }
+    strcpy(nomFichero, nombreFichero);
+
 }
 
-// Método Mostrar
+
+
+// Método Mostrar (Por Hacer)
 void Torneo::mostrar(float hdcp) {
-    if(numGolfistas == 0) {
-        cout << "[!] No hay golfistas inscritos." << endl;
-        return;
-    }
-
-    Golfista* buffer = new Golfista[numGolfistas];
-
-    fichero.seekg(sizeof(int), ios::beg);
-    fichero.read((char*) buffer, numGolfistas * sizeof(Golfista));
-
-    for(int i = 0; i < numGolfistas; i++) {
-        if(hdcp == -1 || buffer[i].handicap == hdcp) {
-            cout << "Licencia: " << buffer[i].licencia << endl
-                 << "Handicap: " << buffer[i].handicap << endl
-                 << "Nombre: " << buffer[i].nombre << " " << buffer[i].apellidos << endl
-                 << "X=================================X" << endl;
-        }
-    }
-    delete[] buffer;
 }
 
-// Método Consultar
+
+
+// Método Consultar (Por Hacer)
 Golfista Torneo::consultar(int posicion) {
-    Golfista g;
-    if(posicion < 1 || posicion > numGolfistas) {
-        return g;
-    }
-
-    fichero.seekg(sizeof(int) + (posicion - 1) * sizeof(Golfista));
-    fichero.read((char*) &g, sizeof(Golfista));
-    return g;
 }
 
-// Método Buscar
+
+
+// Método Buscar (Por Hacer)
 int Torneo::buscar(cadena licencia) {
-    if(numGolfistas == 0) {
-        return -1;
-    }
-
-    Golfista* buffer = new Golfista[numGolfistas];
-    fichero.seekg(sizeof(int), ios::beg);
-    fichero.read((char*) buffer, numGolfistas * sizeof(Golfista));
-
-    for(int i = 0; i < numGolfistas; i++) {
-        if(strcmp(buffer[i].licencia, licencia) == 0) {
-            delete[] buffer;
-            return i + 1;
-        }
-    }
-    delete[] buffer;
-    return -1;
 }
 
-// Método Insertar
+
+
+// Método Insertar (Por Hacer)
+void Torneo::insertar(Golfista g) {
+}
+
+
+
 // Método Modificar
 // Método Eliminar
 // Método Clasifciar
